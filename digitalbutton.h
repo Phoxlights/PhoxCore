4000c3c5c6130035a7cf3b0a131d69ea88f6a755
kwirbx <kwirbx@gmail.com>
2019-07-17 10:20:09 -0500

    Added support for shake
    Added support for shake

--- digitalbutton.h
+++ digitalbutton.h
@@ -4,14 +4,18 @@
 typedef struct DigitalButtonState* DigitalButton;
 typedef void(buttonCallback)();
 
-DigitalButton buttonCreate(int pin, int debounceTime);
+DigitalButton buttonCreate(int pin);
 
-void buttonOnDown(DigitalButton state, buttonCallback cb);
-void buttonOnUp(DigitalButton state, buttonCallback cb);
+void digitalButtonTick(DigitalButton state);
+
+void buttonOnDown(DigitalButton state, buttonCallback cb, int debounceTime);
+void buttonOnUp(DigitalButton state, buttonCallback cb, int debounceTime);
 // handler for quick button down then up
-void buttonOnTap(DigitalButton state, buttonCallback cb);
+void buttonOnTap(DigitalButton state, buttonCallback cb, int debounceTime);
 // handler for button down for extended period of time
 // NOTE - holdMin is in ms
 void buttonOnHold(DigitalButton state, buttonCallback cb, int holdMin);
+//button state, callback function, number of shakes, shake window in milliseconds
+void buttonOnShake(DigitalButton state, buttonCallback cb, int shakeCount, int shakeWin);
 
 #endif
